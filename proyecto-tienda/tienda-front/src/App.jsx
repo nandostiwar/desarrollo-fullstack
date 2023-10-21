ac114a2a43b943eae2f1fdaa9a2d0a502728e5d9
kardzae <emmanuelpp21@gmail.com>
2023-10-21 18:29:49 -0500

    actualizando proyecto parcial 2

--- proyecto-tienda/tienda-front/src/App.jsx
+++ proyecto-tienda/tienda-front/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Form from './components/Form'
 import AdminView from './components/AdminView'
 import MeseroView from './components/MeseroView'
@@ -11,6 +11,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path='/' element={<Navigate to="/login" />}></Route>
         <Route path='/login' element={<Form callback={setUserRol}/>}></Route>
         <Route path='/admin' element={<AdminView userRol={userRol}/>}></Route>
         <Route path='/mesero' element={<MeseroView userRol={userRol}/>}></Route>
