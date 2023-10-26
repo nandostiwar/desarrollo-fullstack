ecdc010c19416d633480a8ccdc85c8b815a30268
piterandres <piteryu83@gmail.com>
2023-10-25 19:01:36 -0500

    eliminar

--- proyecto-local/local-front/src/components/Form.jsx
+++ proyecto-local/local-front/src/components/Form.jsx
@@ -24,6 +24,7 @@ function Form({callback}){
         
     }
     return (
+        
         <div className="container-form">
             <h1>Delicia Express</h1>
             <form onSubmit={handleSubmit}>
