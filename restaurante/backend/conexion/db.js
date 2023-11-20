678b64759af2d02033581badcda39c6b2426fc6e
Hernan <herjosuee@hotmail.com>
2023-11-20 00:52:37 -0500

    Subiendo parcial 3

--- restaurante/backend/conexion/db.js
+++ restaurante/backend/conexion/db.js
@@ -6,7 +6,7 @@ const dbConfig = {
   port: 5432, // Puerto de PostgreSQL
   database: 'Restaurante', // Nombre de tu base de datos
   user: 'postgres', // Nombre de usuario de PostgreSQL
-  password: '1012pocholin', // Contraseña de PostgreSQL
+  password: '', // Contraseña de PostgreSQL
 };
 
 // Crea una instancia de la base de datos
